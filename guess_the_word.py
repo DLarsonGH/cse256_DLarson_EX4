9da04d19ade8e37085de107c08ccbfac3b9f5003
DLarsonGH <dlarsonri@gmail.com>
2024-11-08 17:20:55 -0700

    Added docstrings for class and methods.

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,15 +1,50 @@
+# Donald Larson
+# CIS256 Fall 2024
+# EX 4 (EX 4)
+"""
+Create a word-guessing game where the user must guess a word letter by letter.
+
+Write a Python script named guess_the_word.py that implements the following:
+1. The program selects a random word from a predefined list.
+2. The user guesses one letter at a time.
+3. If the guessed letter is in the word, it is revealed; otherwise, the program
+   indicates the guess is incorrect.
+4. The game continues until the user correctly guesses the entire word or runs
+   out of attempts.
+5. Display a congratulatory message when the word is guessed.
+
+This implementation is designed to help students learn the U.S. state capitals
+and state abbreviations.  Because some city names consist of two or more words
+(e.g. "Santa Fe,NM") the guess characters may include a blank as well as
+a letter.
+"""
 import random
 
 
 class Guess_city:
     """
-    CLass to support guessing a US city name.
+    The class supports guessing a US city name by selecting one letter at a
+    time.  An object of the class is instantiated by calling the constructor
+    with a string of the form "Xxxxxxxxx,YY" where Xxxxxxxxx is the city name
+    and YY is the associated state abbreviation.  On instantiation, a guess_word
+    is created of equal length as the selected city name but with the letters
+    replaced by underscores ("_" ).  When a letter which appears in the selected
+    city name is guessed, the corresponding position(s) in the guess word are
+    changed from "_" to the correct character.  The guess character can also
+    be a blank as some city names are two or more words in length.  Initialize
+    counters for the number guesses and a runnng count of the number of letters
+    correctly guessed.
     """
 
     def __init__(self, city_name):
+        """
+        Instantiate a Guess_city object.  Store the full city name and create
+        a guess word of with the characters (except "'") replaced by "_".
+        :param city_name:
+        """
         # Store the full city name as a list for convenience
         self._city_name = list(city_name)
-        # Initialize the guess string to all blanks (as a list)
+        # Initialize the guess string to all underscores (as a list)
         temp_guess = "_" * len(city_name)
         temp_list = list(temp_guess)
         # Place the comma separator between city and state names
@@ -24,7 +59,6 @@ class Guess_city:
         in the target name and, if so, update the corresponding positions in the
         guess string and increment the number of letters found.  Increment the
         number of guesses count.
-
         :param guess_ch: a lower-case letter [a-z] or a blank (" ")
         :return: True if one or more matches were found
         """
@@ -60,6 +94,19 @@ class Guess_city:
     def num_letters_found(self):
         return self._num_letters_found
 
+    def formatted_guess(self):
+        """
+        Format guess word for better readability.
+        :return: string
+        """
+        wide_word = []
+        for i, ch in enumerate(self._city_guess):
+            wide_word.append(ch)
+            if i < (len(self._city_guess)-1):
+                wide_word.append(" ")
+        return "".join(wide_word)
+
+
 
 filename = "us_state_capitals.csv"
 with open(filename, "r") as file:
@@ -71,12 +118,11 @@ for i in range(2):
     temp_name = encoded_name.replace("#", " ")
     city_name = temp_name.replace("_", ",")
     g = Guess_city(city_name)
-    print(f"Before: "
-          f"{g.city_name}  : {g.city_guess}")
+    print(f"Chose {g.city_name}")
     for ch in ['a', 'e', 'c', ' ']:
         if g.process_guess(ch):
             print(f"Found {ch}!")
         else:
             print(f"Didn't find {ch}")
-    print(f"After: {g.city_name}  : {g.city_guess}")
-    print(f"{g.num_letters_found} in {g.num_guesses} guesses.")
+    print(f"After {g.num_guesses} guesses, {g.num_letters_found} letters found.")
+    print(f"Guess the capital and state: {g.formatted_guess()}.")
